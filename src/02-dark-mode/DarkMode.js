fc8b34d756c06e819006c1658bd45f153fc223d9
Hila Dolev <hiladolev1@gmail.com>
2023-12-31 14:02:24 +0200

    onClick events for setting the darkMode variable and changing the mode variable to the classname for the darkmode

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,27 @@
-export default function DarkMode () {
+import { useState } from "react";
+
+export default function DarkMode() {
+  const [darkModeOn, setDarkMode] = useState(false);
+  const mode = darkModeOn && "dark-mode";
+
+  const handleDarkMode = () => {
+    if (!darkModeOn) {
+      setDarkMode(true);
+    }
+  };
+  const handleLightMode = () => {
+    if (darkModeOn) {
+      setDarkMode(false);
+    }
+  };
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`page ${mode}`}>
+      <button className="dark-mode-button" onClick={handleDarkMode}>
+        Dark Mode
+      </button>
+      <button className="light-mode-button" onClick={handleLightMode}>
+        Light Mode
+      </button>
     </div>
-  )
+  );
 }
