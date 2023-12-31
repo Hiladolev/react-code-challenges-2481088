087d369960a7592685754858930894dda4c6ad76
Hila Dolev <hiladolev1@gmail.com>
2023-12-31 15:17:17 +0200

    installed and imported useForm and its functions

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,29 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-export default function FormValidator () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('')
+export default function FormValidator() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h2>Sign Up!</h2>
-      <label htmlFor='email'>Email</label>
+      <label htmlFor="email">Email</label>
       <input
-        type='text' name='email'
-        onChange={e => setEmail(e.target.value)}
+        type="text"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <label htmlFor='password'>Password</label>
+      <label htmlFor="password">Password</label>
       <input
-        type='password' name='password'
-        onChange={e => setPassword(e.target.value)}
+        type="password"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <label htmlFor='password-confirm'>Confirm Password </label>
+      <label htmlFor="password-confirm">Confirm Password </label>
       <input
-        type='password' name='password-confirm'
-        onChange={e => setPasswordConfirm(e.target.value)}
+        type="password"
+        name="password-confirm"
+        onChange={(e) => setPasswordConfirm(e.target.value)}
       />
-      <input type='submit' value='Submit' />
+      <input type="submit" value="Submit" />
     </form>
-  )
+  );
 }
